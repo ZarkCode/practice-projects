30edfaddc092a539f02243015c6887132d106024
Tejas <tejaswork.contactme@gmail.com>
2026-07-09 20:43:12 +0530

    Adding store and slice for redux toolkit

--- React/2.React-toolkit-tutorial/src/App.jsx
+++ React/2.React-toolkit-tutorial/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/counterSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => {dispatch(increment())}}>Increment</button>
+      <button onClick={() => {dispatch(decrement())}}>Decrement</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
