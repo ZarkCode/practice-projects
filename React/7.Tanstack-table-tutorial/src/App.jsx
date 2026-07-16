eff8839088d6b6ff9689ee14252570d6cb4823a5
Tejas <tejaswork.contactme@gmail.com>
2026-07-16 17:42:28 +0530

    commit to add pagination for the table using tanstack react

--- React/7.Tanstack-table-tutorial/src/App.jsx
+++ React/7.Tanstack-table-tutorial/src/App.jsx
@@ -2,11 +2,23 @@ import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import mockData from "./data/data.json";
-import { ArrowDown, ArrowUpDown, Mail, Phone, User } from "lucide-react";
+import {
+  ArrowUpDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+  Mail,
+  Phone,
+  Search,
+  User,
+} from "lucide-react";
 import { useState } from "react";
 
 const columnHelper = createColumnHelper();
@@ -55,16 +67,42 @@ const columns = [
 const App = () => {
   const [data, setData] = useState(() => [...mockData]);
   const [sorting, setSorting] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState("");
+
   const table = useReactTable({
     data,
     columns,
-    state: { sorting },
+    state: { sorting, globalFilter },
+    initialState: {
+      pagination: {
+        pageSize: 5,
+      },
+    },
     getCoreRowModel: getCoreRowModel(),
+
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
+
+    getPaginationRowModel: getPaginationRowModel(),
   });
   return (
     <div className="flex flex-col min-h-screen max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div className="mb-4 relative">
+        <input
+          value={globalFilter ?? ""}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Search..."
+          className="w-full pl-10 pr-4 border border-gray-300 rounded-md shadow-sm
+        focus:ring-indigo-500 focus:border-indigo-500"
+        />
+        <Search
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-900"
+          size={20}
+        />
+      </div>
       <div className="overflow-x-auto bg-white shadow-md rounded-lg">
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
@@ -109,6 +147,73 @@ const App = () => {
             ))}
           </tbody>
         </table>
+      </div>
+
+      <div className="flex flex-col sm:flex-row justify-between items-center mt-4 text-sm text-gray-700">
+        <div className="flex items-center mb-4 sm:mb-0">
+          <span className="mr-2">Items per page</span>
+          <select
+            className="border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 p-2"
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {[5, 10, 20, 30].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex items-center space-x-2">
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.firstPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <ChevronsLeft size={20} />
+          </button>
+
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <ChevronLeft size={20} />
+          </button>
+
+          <span className="flex items-center">
+            <input
+              type="number"
+              min={1}
+              max={table.getPageCount()}
+              value={table.getState().pagination.pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                table.setPageIndex(page);
+              }}
+              className="w-16 p-2 rounded-md border-gray-300 text-center"
+            />
+            <span className="ml-1">of {table.getPageCount()}</span>
+          </span>
+
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <ChevronRight size={20} />
+          </button>
+
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.lastPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <ChevronsRight size={20} />
+          </button>
+        </div>
       </div>
     </div>
   );
