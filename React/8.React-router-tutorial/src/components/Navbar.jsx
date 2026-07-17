b33d8976fe6a794c6cd92417345dbecf783a53d4
Tejas <tejaswork.contactme@gmail.com>
2026-07-17 16:05:20 +0530

    commit to add react-router-dom and required components been added

---
+++ React/8.React-router-tutorial/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn9NFw_v8QrH0TTBoxIa2UlSNV-GVETVyQqkKftPc-jqPiIJR2PV3nA0I&s=10" alt="" />
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default Navbar;
